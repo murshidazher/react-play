975b96f3f96010cba605b5073030a9ea2cbebd06
murshidazher <murshidazher@yahoo.com>
2020-06-16 21:36:26 +0530

    styles(Button): add button styles

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,10 +2,14 @@ import styled from "styled-components";
 
 const PrimaryButton = styled.button`
   background-color: #202E78;
-  border: 8px solid transparent;
+  border: none;
+  border-radius: 6px;
   color: white;
-  padding: 8px 12px;
-  font-size: 1rem;
+  padding: 0 1rem;
+  font-size: 14px;
+  line-height: 24px;
+  height: 2.5rem;
+  min-width: 2.5rem;
 `;
 
 export default PrimaryButton;
