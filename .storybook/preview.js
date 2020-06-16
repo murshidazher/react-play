f265132604a6f74cbcd8711bd6f659203236f5bf
murshidazher <murshidazher@yahoo.com>
2020-06-17 02:09:55 +0530

    feat(preview): add preview for storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+// configure the decorators here - what we use with addons
+import { addParameters, addDecorator } from "@storybook/react";
+import { withContexts } from "@storybook/addon-contexts/react";
+import { contexts } from "./contexts";
+
+addParameters({
+  // beackground based on theme
+  backgrounds: [
+    { name: "Default theme", value: "#ffffff", default: true },
+    { name: "Dark theme", value: "#050449" }
+  ]
+});
+
+addDecorator(withContexts(contexts));
