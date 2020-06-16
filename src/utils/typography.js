f25dac3bf05ef9aa43b1879d40440deebbc5d9ad
murshidazher <murshidazher@yahoo.com>
2020-06-16 22:41:43 +0530

    feat(typography): add typography

---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+export const primaryFont = '"IBM Plex Mono", monospace';
+
+export const typeScale = {
+  header1: "1.6rem",
+  header2: "1.4rem",
+  header3: "1.2rem",
+  header4: "1.125rem",
+  header5: "1rem",
+  paragraph: "0.875rem",
+  helperText: "0.8rem",
+  copyrightText: "0.7rem"
+};
