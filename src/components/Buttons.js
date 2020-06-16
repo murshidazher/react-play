4a59a56b390697484e42df0fcc7dcd8582fb5615
murshidazher <murshidazher@yahoo.com>
2020-06-16 23:00:12 +0530

    feat(button): add utils dependencies

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,31 +1,29 @@
 import styled from "styled-components";
-
-const primaryIndigo = "#202E78";
+import { defaultTheme, typeScale } from "../utils";
 
 export const Button = styled.button`
-  font-family: "IBM Plex Mono", monospace;
   font-weight: 500;
   border: none;
   border-radius: 6px;
   padding: 0 1rem;
-  font-size: 14px;
+  font-size: ${typeScale.paragraph};
   line-height: 24px;
   height: 2.5rem;
   min-width: 2.5rem;
 `;
 
 export const PrimaryButton = styled(Button)`
-  background-color: ${primaryIndigo};
+  background-color: ${defaultTheme.primaryColor};
   color: white;
 `;
 
 export const SecondaryButton = styled(Button)`
   background: none;
-  color: ${primaryIndigo};
-  border: 2px solid ${primaryIndigo};
+  color: ${defaultTheme.primaryColor};
+  border: 2px solid ${defaultTheme.primaryColor};
 `;
 
 export const TertiaryButton = styled(Button)`
   background: none;
-  color: ${primaryIndigo};
+  color: ${defaultTheme.primaryColor};
 `;
