8c4b1647383379904bc0f0fd40d489bf76c3fbd0
murshidazher <murshidazher@yahoo.com>
2020-06-16 23:02:11 +0530

    feat(Global): add global styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,12 +1,12 @@
 export const primaryFont = '"IBM Plex Mono", monospace';
 
 export const typeScale = {
-  header1: "1.6rem",
-  header2: "1.4rem",
-  header3: "1.2rem",
-  header4: "1.125rem",
-  header5: "1rem",
-  paragraph: "0.875rem",
+  header1: "1.8rem",
+  header2: "1.6rem",
+  header3: "1.4rem",
+  header4: "1.2rem",
+  header5: "1.1rem",
+  paragraph: "1rem",
   helperText: "0.8rem",
   copyrightText: "0.7rem"
-};
+}
