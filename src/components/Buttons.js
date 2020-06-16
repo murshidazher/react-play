ab80b531fae6ef7f42151f66e239902a73e1496f
murshidazher <murshidazher@yahoo.com>
2020-06-16 22:04:01 +0530

    style(button): add button style inheritance

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 
-const PrimaryButton = styled.button`
-  background-color: #202E78;
+const Button = styled.button`
+  font-family: "IBM Plex Mono", monospace;
   border: none;
   border-radius: 6px;
-  color: white;
   padding: 0 1rem;
   font-size: 14px;
   line-height: 24px;
   height: 2.5rem;
   min-width: 2.5rem;
+`;
+
+const PrimaryButton = styled(Button)`
+  background-color: #202E78;
+  color: white;
 `;
 
 export default PrimaryButton;
