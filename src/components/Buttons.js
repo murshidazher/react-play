d457c4a6fe719f806faabfa1f3769dc95a5fde75
murshidazher <murshidazher@yahoo.com>
2020-06-16 21:25:40 +0530

    feat(button): add PrimaryButton

---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const PrimaryButton = styled.button`
+  background-color: #202E78;
+  border: 8px solid transparent;
+  color: white;
+  padding: 8px 12px;
+  font-size: 1rem;
+`;
+
+export default PrimaryButton;
