7f65cc5eeab6d2b612e869c8dbac3baa69e668b6
murshidazher <murshidazher@yahoo.com>
2020-07-18 01:40:02 +0530

    chore(dep): add auto and changelog dep

--- src/index.js
+++ src/index.js
@@ -1,50 +1,10 @@
-import React, { useState } from "react";
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle, defaultTheme, darkTheme } from "./shared";
-import { SignUpModal, PrimaryButton } from "./components";
+import * as colors from './shared/colors';
+import * as typography from './shared/typography';
+import * as themes from './shared/themes';
+import * as global from './shared/global';
 
-const App = () => {
-  const [useDarkTheme, setUseDarkTheme] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  return (
-    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
-      <button
-        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
-        onClick={() => setUseDarkTheme(true)}
-      >
-        Dark theme
-      </button>
-      <button
-        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
-        onClick={() => setUseDarkTheme(false)}
-      >
-        Default theme
-      </button>
-      <PrimaryButton
-        style={{ margin: "0 16px" }}
-        onClick={() => setShowModal(!showModal)}
-      >
-        Show modal
-      </PrimaryButton>
-      <div
-        style={{
-          background: useDarkTheme
-            ? defaultTheme.primaryColor
-            : darkTheme.primaryColor,
-          width: "100vw",
-          height: "90vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-around"
-        }}
-      >
-        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
+export { colors, typography, themes, global };
 
-        <GlobalStyle />
-      </div>
-    </ThemeProvider>
-  );
-};
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+export * from './components/Buttons';
+export * from './components/Modals';
+export * from './components/TextFields';
