0092e178c04e3d839e327d44303db27b5fbc014c
murshidazher <murshidazher@yahoo.com>
2020-07-20 01:19:34 +0530

    feat(button): export all button types

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import * as colors from './shared/colors';
-import * as typography from './shared/typography';
-import * as themes from './shared/themes';
-import * as global from './shared/global';
+export * from './shared/colors';
+export * from './shared/typography';
+export * from './shared/themes';
+export * from './shared/global';
 
-export { colors, typography, themes, global };
-  
 // export * from './assets';
 export * from './components/Buttons';
 // export * from './components/Modals';
